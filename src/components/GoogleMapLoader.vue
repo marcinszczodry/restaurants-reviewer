7e2525cb8184f07ed155a795c23f0ac0ad022419
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-22 04:48:15 +0000

    chore(map_init): load and store 'google' instance

--- src/components/GoogleMapLoader.vue
+++ src/components/GoogleMapLoader.vue
@@ -3,8 +3,23 @@
 </template>
 
 <script>
+import Initializer from 'google-maps-api-loader';
+
 export default {
   name: 'GoogleMapLoader',
+  data() {
+    return {
+      API_KEY: 'AIzaSyD1QsK0TVMn_SWdk0k1K5oLR_DS0_6Cbqo',
+      google: null,
+    };
+  },
+  mounted() {
+    Initializer({
+      apiKey: this.API_KEY,
+    }).then((google) => {
+      this.google = google;
+    });
+  },
 };
 </script>
 
