b90125a3d463ebf699c189e530775b0f75da4d4b
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-22 07:07:27 +0000

    refactor(map_init): load map inside <google-map>
    map is initlized in <google-map> instead of <google-map-loader>
    as components passed inside <google-map> may inherit 'google'
    and 'map' references using 'this.$parent.google' and
    'this.$parent.map'

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
-    <google-map-loader/>
+    <div class="map"></div>
+    <template v-if="!!google && !!map">
+      <slot></slot>
+    </template>
   </div>
 </template>
 
 <script>
-import GoogleMapLoader from './GoogleMapLoader.vue';
+import Initializer from 'google-maps-api-loader';
 
 export default {
   name: 'GoogleMap',
-  components: {
-    GoogleMapLoader,
+  data() {
+    return {
+      API_KEY: 'AIzaSyD1QsK0TVMn_SWdk0k1K5oLR_DS0_6Cbqo',
+      google: null,
+      map: null,
+    };
+  },
+  created() {
+    Initializer({
+      apiKey: this.API_KEY,
+    }).then((google) => {
+      this.google = google;
+      this.initialize();
+    });
+  },
+  methods: {
+    initialize() {
+      const container = this.$el.querySelector('.map');
+      const { Map } = this.google.maps;
+      this.map = new Map(container);
+    },
   },
 };
 </script>
 
-<style scoped>
+<style>
+  .map {
+    height: 100vh;
+    width: 100%;
+  }
 </style>
