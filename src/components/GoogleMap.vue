d525e0276e26dc4f965fdd54700ba0e4b707d40a
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-22 04:25:10 +0000

    chore(map_init): create and insert <google-map-loader>
    core element that will hold all slots passed to <google-map> so
    that the slots will have access to the instance of google and map
    objects

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <google-map-loader/>
+  </div>
 </template>
 
 <script>
+import GoogleMapLoader from './GoogleMapLoader.vue';
+
 export default {
   name: 'GoogleMap',
+  components: {
+    GoogleMapLoader,
+  },
 };
 </script>
 
