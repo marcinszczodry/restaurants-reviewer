30b2f589c9d66c5a268e31cb40661d63f969fcd6
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-22 03:56:32 +0000

    chore(lint): use airbnb styleguide instead of standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,25 @@ module.exports = {
   },
   extends: [
     'plugin:vue/recommended',
-    '@vue/standard',
+    '@vue/airbnb',
   ],
   parserOptions: {
     parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-newline': ["error", {
+      "multiline": "always"
+    }],
+    'vue/html-self-closing': ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "never",
+      "math": "never"
+    }]
   },
 };
