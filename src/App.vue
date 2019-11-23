d45beae69eb82a31cda0fff0be1ca8c18d26e65d
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-23 02:15:15 +0000

    feat(map_start): display google maps with fallback coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <google-map />
+    <google-map
+      :center="mapCenter"
+      :zoom="mapZoom"
+    />
   </div>
 </template>
 
@@ -13,6 +16,23 @@ import GoogleMap from '@/components/GoogleMap.vue';
 export default {
   components: {
     GoogleMap,
+  },
+  data() {
+    return {
+      map: {
+        // by default fallback to england's coordinates
+        center: { lat: 52.3555, lng: -1.1743 },
+        zoom: 7,
+      },
+    };
+  },
+  computed: {
+    mapCenter() {
+      return this.map.center;
+    },
+    mapZoom() {
+      return this.map.zoom;
+    },
   },
 };
 </script>
