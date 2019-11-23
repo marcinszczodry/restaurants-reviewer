02b5413be187ef8914b9e750f23735e2e5088de8
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-23 07:56:37 +0000

    feat: use custom marker icon for user location

--- src/components/GoogleMapAnchor.vue
+++ src/components/GoogleMapAnchor.vue
@@ -1,8 +1,15 @@
 <script>
 import marker from '@/mixins/marker';
+// eslint-disable-next-line import/extensions
+import ICON_USER from '../assets/icons/iconUser.svg?data';
 
 export default {
   name: 'GoogleMapAnchor',
   mixins: [marker],
+  data() {
+    return {
+      icon: ICON_USER,
+    };
+  },
 };
 </script>
