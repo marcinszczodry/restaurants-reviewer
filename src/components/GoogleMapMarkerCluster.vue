7a9c1f0c4f33fdef71ec67f3e3d8645b98243a43
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-25 20:13:54 +0000

    chore: add and invoke method getMarkers
    getMarkers() returns Marker instances from the slot ($data.marker)
    
    invoked on mounted() and assigned to $data.markers

--- src/components/GoogleMapMarkerCluster.vue
+++ src/components/GoogleMapMarkerCluster.vue
@@ -10,7 +10,16 @@ export default {
     return {
       google: this.$parent.google,
       map: this.$parent.map,
+      markers: [],
     };
+  },
+  mounted() {
+    this.markers = this.getMarkers();
+  },
+  methods: {
+    getMarkers() {
+      return this.$children.map((m) => m.marker);
+    },
   },
   render: () => null,
 };
