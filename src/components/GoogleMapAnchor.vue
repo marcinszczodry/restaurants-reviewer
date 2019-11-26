46506062ef3603393c59f56662f5e18019f865b2
Marcin Szczodry <howdy.marcin@gmail.com>
2019-11-26 07:22:55 +0000

    chore: add methods to build and manipulate the circle

--- src/components/GoogleMapAnchor.vue
+++ src/components/GoogleMapAnchor.vue
@@ -15,7 +15,37 @@ export default {
   data() {
     return {
       icon: ICON_USER,
+      circle: null,
     };
+  },
+  methods: {
+    initializeCircle() {
+      const { Circle } = this.google.maps;
+      return new Circle({
+        strokeColor: '#0057FF',
+        strokeOpacity: 0.8,
+        strokeWeight: 0.5,
+        fillColor: '#0057FF',
+        fillOpacity: 0.18,
+        map: this.map,
+        center: this.position,
+        radius: this.range,
+      });
+    },
+    appendCircle(circleInstance) {
+      circleInstance.setMap(this.map);
+    },
+    removeCircle(circleInstance) {
+      circleInstance.setMap(null);
+    },
+    createCircle() {
+      this.circle = this.initializeCircle();
+      this.appendCircle(this.circle);
+    },
+    swapCircle() {
+      this.removeCircle(this.circle);
+      this.createCircle();
+    },
   },
 };
 </script>
