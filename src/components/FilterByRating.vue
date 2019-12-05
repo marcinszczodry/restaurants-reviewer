c320a131569f0a56fff8e2494df3a42970a58c00
Marcin Szczodry <howdy.marcin@gmail.com>
2019-12-05 18:35:20 +0000

    feat: allow to filter restaurants by min and max rating

---
+++ src/components/FilterByRating.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="minmax">
+    <div>
+      min rating: {{ minValue }} {{ +minValue === 1 ? 'star' : 'stars' }}<br />
+      <label>
+        <input
+          v-model="minValue"
+          type="range"
+          :min="1"
+          :max="maxValue"
+          @click="$emit('min', minValue)"
+        />
+      </label>
+    </div>
+    <div>
+      max rating: {{ maxValue }} {{ +maxValue === 1 ? 'star' : 'stars' }}<br />
+      <label>
+        <input
+          v-model="maxValue"
+          type="range"
+          :min="minValue"
+          :max="5"
+          @click="$emit('max', maxValue)"
+        />
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterByRating',
+  props: {
+    min: {
+      type: Number,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      minValue: this.min,
+      maxValue: this.max,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .minmax {
+    display: inline-flex;
+    >:first-child:not(:last-child) {
+      margin-right: 20px;
+    }
+  }
+</style>
